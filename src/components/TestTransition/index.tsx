e592ed57aeb77753d49d78a5cf75b3dbcfb391c7
Human Town <ty.1997@foxmail.com>
2022-06-28 15:58:31 +0800

    选择animejs作为vue3 tsx的动画库

--- src/components/TestTransition/index.tsx
+++ src/components/TestTransition/index.tsx
@@ -1,23 +1,45 @@
-import { defineComponent, ref, Transition } from "vue";
-import "./index.module.scss";
+import { defineComponent, Fragment, onMounted, ref } from "vue";
+import anime from "animejs/lib/anime.es.js";
+import "./index.scss";
 const Index = defineComponent({
   setup() {
-    const show = ref(true);
-    const Slots = {
-      default: () => show.value && <p>hello</p>,
+    const myObject = {
+      prop1: 0,
+      prop2: "0%",
     };
+    const inner = ref<HTMLDivElement | null>(null);
+    onMounted(() => {
+      anime({
+        targets: myObject,
+        prop1: 50,
+        prop2: "100%",
+        easing: "linear",
+        round: 1,
+        update: function () {
+          inner.value!.innerHTML = JSON.stringify(myObject);
+        },
+      });
+      anime({
+        targets: ".inner2",
+        width: "100%", // -> from '28px' to '100%',
+        easing: "easeInOutQuad",
+        direction: "alternate",
+        loop: true,
+      });
+      anime({
+        targets: ".btn",
+        translateX: [100, 250], // from 100 to 250
+        delay: 500,
+        direction: "alternate",
+        loop: true,
+      });
+    });
     return () => (
-      <>
-        <h1>↓↓↓测试transition ↓↓↓</h1>
-        <button
-          onClick={() => {
-            show.value = !show.value;
-          }}
-        >
-          Toggle
-        </button>
-        <Transition>{Slots}</Transition>
-      </>
+      <div class={"outer"}>
+        <div class={"inner"} ref={inner}></div>
+        <div class={"inner2"}></div>
+        <div class={"btn"}></div>
+      </div>
     );
   },
 });
